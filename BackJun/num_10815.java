c0dba61fbe548338ec5c42147683f6c9d103326b
Yoon Do Yeong <ehdud05264@naver.com>
2025-03-18 13:59:43 +0900

    백준 10815 풀이

---
+++ BackJun/num_10815.java
@@ -0,0 +1,45 @@
+package BackJun;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int c1 = Integer.parseInt(br.readLine());
+        int[] ar1 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int c2 = Integer.parseInt(br.readLine());
+        int[] ar2 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        StringBuilder sb = new StringBuilder();
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < c1; i++){
+            map.put(String.valueOf(ar1[i]), map.getOrDefault(String.valueOf(ar1[i]), 0) + 1);
+        }
+
+        for(int i = 0; i < c2; i++){
+            map.put(String.valueOf(ar2[i]), map.getOrDefault(String.valueOf(ar2[i]), 0) + 1);
+        }
+
+        for(int i = 0; i < c2; i++){
+            if(map.get(String.valueOf(ar2[i])) == 2){
+                sb.append(1).append(" ");
+            } else {
+                sb.append(0).append(" ");
+            }
+        }
+
+        String answer = sb.toString().trim();
+        System.out.println(answer);
+
+        br.close();
+    }
+}
+
+
+
