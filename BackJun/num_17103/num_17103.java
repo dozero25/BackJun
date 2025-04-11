bb3afd24bfc8c8a6c7d6cd388e82cce24a15a5b8
Yoon Do Yeong <ehdud05264@naver.com>
2025-04-11 17:53:44 +0900

    백준 17103(실버 2) Java 풀이 추가

--- BackJun/num_17103/num_17103.java
+++ BackJun/num_17103/num_17103.java
@@ -48,6 +48,64 @@ public class num_17103 {
     }
 }
 
+public class num_17103_2 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testcase = Integer.parseInt(br.readLine());
+        // 2-1.
+        int[] input = new int[testcase];
+        int max = 0;
+
+        for(int i = 0; i < testcase; i++){
+            input[i] = Integer.parseInt(br.readLine());
+            if(input[i] > max){
+                max = input[i];
+            }
+        }
+
+        boolean[] isNotPrime = new boolean[max + 1];
+        calcPrime(isNotPrime);
+
+        // 2-2.
+        for(int n : input){
+            int count = 0;
+            for(int i = 2; i <= n/2; i++){
+                if(!isNotPrime[i] && !isNotPrime[n - i]){
+                    count++;
+                }
+            }
+            bw.write(count + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void calcPrime(boolean[] isNotPrime){
+        isNotPrime[0] = isNotPrime[1] = true;
+
+        int n = isNotPrime.length;
+
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(!isNotPrime[i]){
+                for(int j = i * i; j < n; j += i){
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
 
 
 
